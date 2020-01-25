312b64a1b0a7bbe123780828dbf396239cad36f6
Xavier Perez <xperez007@gmail.com>
2020-01-25 20:54:46 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,3 +1,28 @@
+#add_element_to_end_of_array
+#push
+#add_element_to_start_of_array
+#unshift
+#remove_element_from_end_of_array
+#pop
+#remove_element_from_start_of_array
+#unshift
+#remove_element_from_start_of_array
+#unshift
+#retrieve_element_from_index
+[]
+#retrieve_first_element_from_array
+[]
+#retrieve_last_element_from_array
+[]
+-1
+
+
+#create_an_array
+end
+
+def create_an_empty_array
+  []
+end
 def create_an_empty_array
   
 end
